ff6299add78e8ee96f34502cc6fb72629a02c9f3
diego-avalos <diego-avalos@PRO-PAE-5234.PROCERGS.REDERS>
2021-02-08 15:35:43 -0300

    Test Pauta Controller

--- sicredi/src/test/java/com/avalos/sicredi/controller/PautaControllerTest.java
+++ sicredi/src/test/java/com/avalos/sicredi/controller/PautaControllerTest.java
@@ -1,6 +1,10 @@
 package com.avalos.sicredi.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.net.URI;
 
@@ -11,8 +15,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.avalos.sicredi.form.PautaForm;
+import com.avalos.sicredi.model.StatusPauta;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,49 +27,45 @@ class PautaControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
 	
 	@Test
-	void deveCadastrarPauta() throws Exception {
-		URI uri = new URI("/pautas/v2");
+	void case1__cadastraPauta() throws Exception {
+		PautaForm pautaForm = new PautaForm();
+		pautaForm.setTitulo("Teste Controller");
+		pautaForm.setDescricao("Teste Controller Descrição");
 
-		String json = 
-				"{"
-				+ 	"\"descricao\": \"Pauta para teste \", "
-				+ 	"\"titulo\": \"Pauta 1\""
-				+ "}";
+		mockMvc.perform(
+				post("/pautas/v2")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(pautaForm)))
+				.andExpect(status().isCreated());
 
-		mockMvc
-			.perform(
-					MockMvcRequestBuilders
-					.post(uri)
-					.content(json)
-					.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.status().isCreated());
 	}
-	
+
 	@Test
-	void deveDetalharPauta() throws Exception {
+	void case2__detalhaPauta() throws Exception {
 		URI uri = new URI("/pautas/v2/1");
 
-		mockMvc
-			.perform(
-					MockMvcRequestBuilders
-					.get(uri)
+		mockMvc.perform(
+				get(uri)
 					.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(jsonPath("$.titulo").value("Pauta 1"));
+					.andExpect(status().isOk())
+				.andExpect(jsonPath("$.titulo").value("Teste Controller"));
 	}
-	
+
 	@Test
-	void devAbrirSessao() throws Exception {
+	void case3__abreSessao() throws Exception {
 		URI uri = new URI("/pautas/v2/1/abrirSessao");
 
-		mockMvc
-			.perform(
-					MockMvcRequestBuilders
-					.put(uri)
+		mockMvc.perform(
+				put(uri)
 					.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.status().isOk());
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.titulo").value("Teste Controller"))
+				.andExpect(jsonPath("$.status").value(StatusPauta.OPENED.toString()));
 	}
 
 }
